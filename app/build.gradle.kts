f6865bb14a2c0beeb0c370e10933e863184623ca
nicholasg2001 <nicholasgutierrez62001@gmail.com>
2023-12-27 14:00:15 -0500

    added detail screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+
+
     //Compose ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
